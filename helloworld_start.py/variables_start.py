0112ae149e635c84e84e8e099a9e5b5f82f6775b
Eodera1 <ericowino8@gmail.com>
2024-07-08 20:49:39 +0300

    Python variables

---
+++ helloworld_start.py/variables_start.py
@@ -0,0 +1,16 @@
+# Basic data types in Python: Numbers, Strings, Lists, Tuples, Dictionaries
+myint = 5
+myfloat = 13.2
+mystr = "This is a string"
+mybool = True
+mylist = [0, 1, "two" 3.2, False]
+mytuple = (0, 1, 2)
+mydict = {"a": 1, "b": 2, "c": 3}
+
+print(myint)
+print(myfloat)
+print(mystr)
+print(mybool)
+print(mylist)
+print(mytuple)
+print(mydict)
