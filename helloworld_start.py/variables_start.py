e2082b3e3a077eeff7df43628f818763532d2393
Eodera1 <ericowino8@gmail.com>
2024-07-08 21:19:25 +0300

    Python function

--- helloworld_start.py/variables_start.py
+++ helloworld_start.py/variables_start.py
@@ -3,14 +3,35 @@ myint = 5
 myfloat = 13.2
 mystr = "This is a string"
 mybool = True
-mylist = [0, 1, "two" 3.2, False]
+mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
-mydict = {"a": 1, "b": 2, "c": 3}
+mydict = {"one": 1, "b": 2, "c": 3}
 
-print(myint)
-print(myfloat)
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
+
+# # re-declaring a variable works
+# myint = "abc"
+# print(myint)
+
+# dictionaries are accessed via keys
+# print(mydict["one"])
+
+# ERROR: variables of different types cannot be combined
+print("string type " + str(123))
+
+# Global vs. local variables in functions
+def someFunction():
+    global mystr
+    mystr = "def"
+    print(mystr)
+
+someFunction()
 print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+
+
