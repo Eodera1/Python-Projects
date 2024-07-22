fbb75f3ba9c9d3114e873c2d2a7818d9e26dc2bc
Eodera1 <ericowino8@gmail.com>
2024-07-22 19:24:55 +0300

    Python twinkle twinkle

--- __pycache__/comments.py
+++ __pycache__/comments.py
@@ -1,5 +1,12 @@
+'''
+Author: Erick
+Date:
+Time:
+'''
+
 import time
 
 print('Waiting for 1 second')
+# Feel free to change the delay as per the instructions from your boss
 time.sleep(1)
-print('Waiting for 1 second completed')
+print('Waiting for 1 second completed')       
